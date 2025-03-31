c361df38359b322abac868742db38993d9c18df6
NewPhiphat <phiphat.tjr@tupp.ac.th>
2025-03-31 16:55:38 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let queues = {
+    plan1: 0,
+    plan2: 0,
+    plan3: 0,
+    plan4: 0,
+    plan5: 0,
+    plan6: 0
+};
+
+function callQueue(plan) {
+    // เพิ่มคิวให้แผนการเรียนนั้น
+    queues[plan]++;
+    
+    // แสดงคิวปัจจุบัน
+    document.getElementById(`queue-${plan}`).textContent = `คิวปัจจุบัน: ${queues[plan]}`;
+}
